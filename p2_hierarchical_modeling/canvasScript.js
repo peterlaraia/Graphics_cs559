0d3aa484fcb5a755f40f2fb944554c3d78c860ea
inetrica <inetrica@gmail.com>
2016-02-07 11:42:10 -0600

    magic numbers put in variables, modified structure to clean up drawPicture

--- p2_hierarchical_modeling/canvasScript.js
+++ p2_hierarchical_modeling/canvasScript.js
@@ -1,3 +1,21 @@
+//INITIAL VALUES
+const _P1_BICEP_ANGLE = -30;
+const _P1_FOREARM_ANGLE=-25;
+const _P2_BICEP_ANGLE = 35;
+const _P2_FOREARM_ANGLE = -10;
+
+const _PERSON_X = 125;
+const _PERSON_Y = 325;
+const _TABLE_X = 250;
+const _TABLE_Y = 400;
+
+const _BALL_X = 165;
+const _BALL_X_VEL = 29.5;
+const _BALL_X_VEL_MODIFIER = .8;
+const _BALL_Y_VEL = -35;
+const _BALL_Y_VEL_SUB = -22;
+
+
 window.onload = function() {
 	var canvas = document.getElementById('myCanvas');
 	var context = canvas.getContext('2d');
@@ -8,9 +26,15 @@ window.onload = function() {
 	net = new Limb(8, 17, 'rgba(25, 25, 25, 0.4)')
 
 	pptable = new PingPongTable(pptable, leg1, leg2, net);
-	ball = new Ball(0, 0, 6, 29.5, -35, 'rgb(0, 220, 220)');
+	ball = new Ball(0, 0, 6, _BALL_X_VEL, _BALL_Y_VEL, 'rgb(0, 220, 220)');
 
-	drawPicture(context, canvas, -30, -25, 30, -15, 3, pptable, ball, 0);
+	person1 = new Person(_P1_BICEP_ANGLE, _P1_FOREARM_ANGLE);
+	person2 = new Person(_P2_BICEP_ANGLE, _P2_FOREARM_ANGLE);
+
+	
+
+	//drawPicture(context, canvas, -30, -25, 30, -15, 3, pptable, ball, 0);
+	drawPicture(context, canvas, person1, person2, 3, pptable, ball, 0);
 };
 
 function Limb(w, h, col){
@@ -94,7 +118,7 @@ function drawFullPingPongTable(pptable, context){
 
 
 	context.save(); //origin save
-	context.translate(250, 400);
+	context.translate(_TABLE_X, _TABLE_Y);
 	drawTable(pptable.table, context);
 	context.save(); //table nw save
 	
@@ -112,12 +136,13 @@ function drawFullPingPongTable(pptable, context){
 
 }
 
-function drawLeftPerson(context, luTheta, lfTheta){
-	person = new Person(luTheta, lfTheta);
+//function drawLeftPerson(context, luTheta, lfTheta){
+function drawPerson(context, person){
+	//person = new Person(luTheta, lfTheta);
 	
 	//draw the head
 	context.save(); //origin save
-	context.translate(125, 325);
+	context.translate(_PERSON_X, _PERSON_Y);
 	context.beginPath();
 	context.arc(0, 0, person.head.r, 0, 2*Math.PI);
 	context.lineWidth=5;
@@ -156,7 +181,7 @@ function drawLeftPerson(context, luTheta, lfTheta){
 	context.restore();
 }
 
-function drawPicture(context, canvas, p1Upper, p1Fore, p2Upper, p2Fore, angleVel, pptable, ball, timer){
+function drawPicture(context, canvas, person1, person2, angleVel, pptable, ball, timer){
 	//clear board then redraw for animation
 	context.save();
 	context.setTransform(1, 0, 0, 1, 0, 0);
@@ -169,9 +194,9 @@ function drawPicture(context, canvas, p1Upper, p1Fore, p2Upper, p2Fore, angleVel
 	//If the ball hits the table 
 	if(ball.y + ball.r > 7 /*&& (ball.x > (250 -165) && ball.x < (250-165 + 400))*/){
 		timer = 0;
-		if((ball.xVel > 0 && ball.x > (250 - 165) + pptable.table.width/2) || (ball.xVel < 0 && ball.x < (250 - 165) + pptable.table.width/2)){ //halfway across table
-			ball.y0Vel = -22;
-			ball.xVel *= .8;
+		if((ball.xVel > 0 && ball.x > (_TABLE_X - _BALL_X) + pptable.table.width/2) || (ball.xVel < 0 && ball.x < (_TABLE_X - _BALL_X) + pptable.table.width/2)){ //halfway across table
+			ball.y0Vel = _BALL_Y_VEL_SUB;
+			ball.xVel *= _BALL_X_VEL_MODIFIER;
 			ball.y = ball.y0Vel * timer  + (4.9*timer*timer);
 		} else ball.y = 0;
 		//ball.y = 0;
@@ -179,40 +204,40 @@ function drawPicture(context, canvas, p1Upper, p1Fore, p2Upper, p2Fore, angleVel
 	}
 
 	//if the ball reaches either player
-	if(ball.x < 0 || ball.x > ((250-165)*2 + pptable.table.width) ) {
+	if(ball.x < 0 || ball.x > ((_TABLE_X-_BALL_X)*2 + pptable.table.width) ) {
 		ball.x -= ball.xVel;
 		angleVel *= -1;
 		timer = 0;
-		ball.y0Vel = -35;
-		ball.xVel = 29.5;
+		ball.y0Vel = _BALL_Y_VEL;
+		ball.xVel = _BALL_X_VEL;
 		if(angleVel < 0){
 			ball.xVel *= -1;
 		}
 	}
 
-	
-	//-30.20
-	//-25.-15
 	drawFullPingPongTable(pptable, context);
-	drawLeftPerson(context, p1Upper, p1Fore);
+	drawPerson(context, person1);
 
 	context.save(); //save origin
 	
 	context.translate(canvas.width,0);
 	context.scale(-1, 1);
-	drawLeftPerson(context, p2Upper, p2Fore);
+	drawPerson(context, person2);
 	context.restore(); //origin ret
 
 	context.save();//save origin
-	context.translate(165, 395);
+	context.translate(_BALL_X, 395);
 	context.translate(ball.x, ball.y);
 	drawBall(ball, context);
 	context.restore();//origin ret
 	
 	ball.x += ball.xVel;
+	person1.luArm.theta += angleVel;
+	person1.lfArm.theta += (angleVel*.5);
+	person2.luArm.theta -= angleVel;
+	person2.lfArm.theta -= (angleVel*.5);
 
-	setTimeout(drawPicture, 40, context, canvas, p1Upper + angleVel, p1Fore + (angleVel/2),
-			p2Upper - angleVel, p2Fore -(angleVel/2), angleVel, pptable, ball, timer + 1);
+	setTimeout(drawPicture, 40, context, canvas, person1, person2, angleVel, pptable, ball, timer + 1);
 }
 
 function a2r(angle){
